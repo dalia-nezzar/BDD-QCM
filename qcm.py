da7c881662aaee3e9970e1f15d48411908e3b335
Dalia Nezzar <113182098+dalia-nezzar@users.noreply.github.com>
2025-06-12 11:01:46 +0200

    fix(QCM): wal question error

--- qcm.py
+++ qcm.py
@@ -60,7 +60,7 @@ questions = [
             "Stocker les données utilisateur",
             "Gérer les connexions utilisateur"
         ],
-        "correct": 2,
+        "correct": 1,
         "explanation": "Le WAL (Write Ahead Log) garantit qu'une instance de base de données puisse survivre aux pannes en cas de crash du système."
     },
     {
